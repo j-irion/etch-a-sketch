da56d1fea595b06488accdbf7006f66d722dad6a
j-irion <juliusirion@gmx.de>
2022-07-12 22:01:04 +0200

    Add hover and color function

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ function createGrid(size) {
     field.style.width = `${grid.clientHeight / size}px`;
     grid.appendChild(field);
   }
+  createHoverFunctionality();
+}
+
+function createHoverFunctionality() {
+  let fields = document.querySelectorAll(".field");
+  fields.forEach((field) => {
+    field.addEventListener("mouseover", () => {
+      field.style.backgroundColor = giveRandomColor();
+    });
+  });
+}
+
+function giveRandomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red},${green},${blue})`;
 }
